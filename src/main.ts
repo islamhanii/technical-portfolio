9508ee291afb343dbc77b2b0352bbbf8e2202c58
islamhanii <islamhani433@gmail.com>
2026-08-30 08:59:06 +0300

    feat: implement GitHub Pages redirect script, update main entry point to main.ts, and add custom 404 page

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import './assets/main.css'
+import router from './router' // 1. Import router
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(router) // 2. Tell Vue to use the router
+
+app.mount('#app')
